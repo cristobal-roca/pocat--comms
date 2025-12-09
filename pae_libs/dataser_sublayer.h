22b5bc95adc65d1d759b58dd5ea5b00e02e8604d
cristobal-roca <cristobal.roca@estudiantat.upc.edu>
2025-12-09 12:40:45 +0100

    librerias de cada capa y funciones en general

---
+++ pae_libs/dataser_sublayer.h
@@ -0,0 +1,9 @@
+#ifndef DATASER_SUBLAYER_H
+#define DATASER_SUBLAYER_H
+
+#include "protocol_definitions.h"
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#endif // DATASER_SUBLAYER_H
